52c8b3e44675afbb1fed4cc93db2708ea3c5ebcc
Courtney Lippman <courtney.lippman@colorado.edu>
2022-08-03 18:07:28 -0600

    Call different array methods for the 3 arrays and make a comment about what impact each method has.

--- arrays.js
+++ arrays.js
@@ -1,5 +1,19 @@
-var pets = ["dog", "cat", "fish", "horse"]
+var pets = ["dog", "cat", "fish", "horse"];
+/* pop() method removes the last element("horse") from an array and returns that
+ element.*/
+pets.pop();
+console.log(pets);
+
+
 
 var ages = [1, 16, 55, 87]
+/* push() method adds one or more elements (22) to the end of an array and
+returns the length of the new array.*/
+ages.push(22);
+console.log(ages);
 
 var trueFalse = [true, false, true, false]
+/* shift() method removes the first element (true) from an array and returns
+ that removed element. */
+trueFalse.shift();
+  console.log (trueFalse);
